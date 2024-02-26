578fa65ba59fcf03b315de99784d1d0185462fdd
Andy Ricardo Galarza Morales <134077120+Ricardo4777@users.noreply.github.com>
2024-02-26 01:08:26 -0500

    Update views2.py
    vistas de el modulo quejas - solo con error de actualizacion

--- Proyecto/tienda/views2.py
+++ Proyecto/tienda/views2.py
@@ -1,3 +1,5 @@
+#Establecer ruta de acceso a la vista de la aplicación ejecutar algo cunado una url sea visitada
+from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 #utilizando esta clase cuando yo lo ejecute me va devolver un formulario 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
@@ -8,68 +10,49 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 # Importar excepción para manejar errores de integridad en la base de datos
 
-from django.db import IntegrityError
+from django.db import IntegrityError # Maneja la excepción de integridad
+
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required
-from .models import Task
+from django.contrib.auth.decorators import login_required #decorador en cada funcion apra proteger
+
 from .models import Queja
-from .forms import TaskForm
+
 from .forms import QuejaForm
 # Create your views here.
-
-
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html', {"form": UserCreationForm})
+        return render(request, 'signup.html', {"form": UserCreationForm}) 
     else:
 
-        if request.POST["password1"] == request.POST["password2"]:
+        if request.POST["password1"] == request.POST["password2"]: #si las contraseñas coinciden entonces los guardamos
+
             try:
                 user = User.objects.create_user(
                     request.POST["username"], password=request.POST["password1"])
                 user.save()
                 login(request, user) #user que queremos guardar
-                return redirect('tasks')
+                return redirect('quejas')
             except IntegrityError:
-                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+                return render(request, 'signup.html', {"form": UserCreationForm, "error": "El usuario ya existe."})
 
-        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+        return render(request, 'signup.html', {"form": UserCreationForm, "error": "contraseñas no coinciden."})
+
 
 
 @login_required
-def tasks(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'tasks.html', {"tasks": tasks})
-
 def quejas(request):
-    quejas = Queja.objects.filter(user=request.user, datecompletedqueja__isnull=True)
-    return render(request, 'quejas.html', {"quejas": quejas})
+    quejas = Queja.objects.filter(user=request.user, datecompletedqueja__isnull=True) # me devuelve todas las quejas que esten en la base de datos
+    return render(request, 'quejas.html', {"quejas": quejas})# pasa el dato al frontend
 @login_required
-def tasks_completed(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'tasks.html', {"tasks": tasks})
 def quejas_completed(request):
-    quejas = Queja.objects.filter(user=request.user, datecompletedqueja__isnull=False).order_by('-datecompleted')
+    quejas = Queja.objects.filter(user=request.user, datecompletedqueja__isnull=False).order_by('-datecompletedqueja')
     return render(request, 'quejas.html', {"quejas": quejas})
 
-@login_required
-def create_task(request):
-    if request.method == "GET":
-        return render(request, 'create_task.html', {"form": TaskForm})
-    else:
-        try:
-            form = TaskForm(request.POST)
-            new_task = form.save(commit=False)
-            new_task.user = request.user
-            new_task.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
+
 
 
 def home(request):
     return render(request, 'home.html')
-
 
 @login_required
 def signout(request):
@@ -83,77 +66,53 @@ def signin(request):
     else:
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
+    if user is None:
             return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
 
-        login(request, user)
-        return redirect('tasks')
+    login(request, user)
+    return redirect('quejas')
 
 @login_required
-def task_detail(request, task_id):
-    if request.method == 'GET':
-        task = get_object_or_404(Task, pk=task_id, user=request.user)
-        form = TaskForm(instance=task)
-        return render(request, 'task_detail.html', {'task': task, 'form': form})
-    else:
-        try:
-            task = get_object_or_404(Task, pk=task_id, user=request.user)
-            form = TaskForm(request.POST, instance=task)
-            form.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
 def queja_detail(request, queja_id):
     if request.method == 'GET':
         queja = get_object_or_404(Queja, pk=queja_id, user=request.user)
         form = QuejaForm(instance=queja)
-        return render(request, 'queja_detail.html', {'queja': Queja, 'form': form})
+        return render(request, 'queja_detail.html', {'queja': queja, 'form': form})
     else:
         try:
             queja = get_object_or_404(Queja, pk=queja_id, user=request.user)
-            form = QuejaForm(request.POST, instance=queja)
+            form = QuejaForm(request.POST, instance=Queja)
             form.save()
             return redirect('quejas')
         except ValueError:
             return render(request, 'queja_detail.html', {'queja': queja, 'form': form, 'error': 'Error updating queja.'})        
 
+# @login_required
 @login_required
-def complete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    if request.method == 'POST':
-        task.datecompleted = timezone.now()
-        task.save()
-        return redirect('tasks')
-    
 def complete_queja(request, queja_id):
     queja = get_object_or_404(Queja, pk=queja_id, user=request.user)
-    if request.method == 'POST':
-        queja.datecompleted= timezone.now()
+    if request.method == 'POST': # si es post lo actualiza
+        queja.datecompletedqueja= timezone.now() # si tiene una fecha es que ya se cumplio que la queja esta completa
         queja.save()
         return redirect('quejas')
 @login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    if request.method == 'POST':
-        task.delete()
-        return redirect('tasks')
 def delete_queja(request, queja_id):
     queja = get_object_or_404(Queja, pk=queja_id, user=request.user)
     if request.method == 'POST':
         queja.delete()
-        return redirect('quejas')
-    
-#-------------------
+    return redirect('quejas')
+
+# #-------------------
 @login_required
 def create_queja(request):
-    if request.method == "GET":
-        return render(request, 'create_queja.html', {"form": QuejaForm})
-    else:
-        try:
-            form = QuejaForm(request.POST)
-            new_queja = form.save(commit=False)
-            new_queja.user = request.user
-            new_queja.save()
-            return redirect('quejas')
-        except ValueError:
-            return render(request, 'create_queja.html', {"form": QuejaForm, "error": "Error creating QUEJA."})
+        if request.method == "GET":
+            return render(request, 'create_queja.html', {"form": QuejaForm})
+        else:
+            try:
+                form = QuejaForm(request.POST)
+                new_queja = form.save(commit=False) #que me devuela los datos que estan dentro del formulario
+                new_queja.user = request.user
+                new_queja.save()
+                return redirect('quejas') #redireccionamos
+            except ValueError:
+                return render(request, 'create_queja.html', {"form": QuejaForm, "error": "Error creating QUEJA."})
